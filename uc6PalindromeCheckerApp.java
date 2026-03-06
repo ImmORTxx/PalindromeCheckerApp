0a3efdb24f1dcfc7de7da9002338146e7db24a8c
ImmORTxx <jinaysakariya@gmail.com>
2026-03-06 22:50:49 +0530

    UC6 implemented

---
+++ uc6PalindromeCheckerApp.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class uc6PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String word = "madam";
+
+        Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
+
+        // Push to stack and enqueue to queue
+        for (int i = 0; i < word.length(); i++) {
+            stack.push(word.charAt(i));
+            queue.add(word.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        // Compare pop and dequeue
+        while (!stack.isEmpty()) {
+            if (stack.pop() != queue.remove()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
+    }
+}
